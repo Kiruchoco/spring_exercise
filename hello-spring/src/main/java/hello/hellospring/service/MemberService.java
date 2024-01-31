c77cddefe3a89e6f123faee1046af722ebd49d82
Kiruchoco <sba1013kr@naver.com>
2024-01-31 16:12:14 +0900

    Session Final clear

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -6,10 +6,12 @@ import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
     // private final MemberRepository memberRepository = new MemoryMemberRepository();
 
@@ -21,9 +23,19 @@ public class MemberService {
 
     // 회원가입
     public long join(Member member){
-        validateDuplicateMember(member);        // 중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+//        long start=System.currentTimeMillis();
+
+        try{
+            validateDuplicateMember(member);        // 중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        }finally{
+//            long finish= System.currentTimeMillis();
+//            long timeMs=finish-start;
+//            System.out.println("join= "+timeMs+"ms");
+        }
+
+
     }
 
     // 같은 이름이 있는 중복 회원 제거
@@ -36,7 +48,15 @@ public class MemberService {
     
     // 전체 회원 조회
     public List<Member> findMember(){
-        return memberRepository.findAll();
+//        long start = System.currentTimeMillis();
+        try{
+            return memberRepository.findAll();
+        }finally {
+//            long finish=System.currentTimeMillis();
+//            long timeMs=finish-start;
+//            System.out.println("findMembers "+timeMs+"ms");
+        }
+
     }
 
     public Optional<Member> findOne(Long memberId){
--- hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,20 +1,51 @@
 package hello.hellospring.service;
 
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.aop.TimeTraceApp;
+import hello.hellospring.repository.*;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
+
+//    private DataSource dataSource;
+//
+//    @Autowired
+//    public SpringConfig(DataSource dataSource){
+//        this.dataSource=dataSource;
+//    }
+
+    // private EntityManager em;
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
+
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    public MemberService memberService(){
+        return new MemberService(memberRepository);
     }
+
+//    @Bean
+//    public TimeTraceApp timeTraceApp(){
+//        return new TimeTraceApp();
+//    }
+
+//    @Bean
+//    public MemberRepository memberRepository()
+//    {
+//        // return new MemoryMemberRepository();
+//        // return new JdbcMemberRepository(dataSource);
+//        // return new JdbcTemplateMemberRepository(dataSource);
+//        // return new JpaMemberRepository(em);
+//
+//
+//    }
 
 }
