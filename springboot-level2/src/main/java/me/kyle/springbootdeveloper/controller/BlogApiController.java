351cc07ca7a4b2d2a900cf60e979479de6453107
Kiruchoco <sba1013kr@naver.com>
2024-02-01 16:01:16 +0900

    to 6.4.3(148) clear

--- springboot-level2/src/main/java/me/kyle/springbootdeveloper/controller/BlogApiController.java
+++ springboot-level2/src/main/java/me/kyle/springbootdeveloper/controller/BlogApiController.java
@@ -3,12 +3,16 @@ package me.kyle.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.kyle.springbootdeveloper.domain.Article;
 import me.kyle.springbootdeveloper.dto.AddArticleRequest;
+import me.kyle.springbootdeveloper.dto.ArticleResponse;
 import me.kyle.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,5 +26,15 @@ public class BlogApiController {
         // 요청한 자원이 성공적으로 생성되었으며, 저장된 블로그 글 정보를 응답 객체에 담아 전송함
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(saveArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok().body(articles);
     }
 }
