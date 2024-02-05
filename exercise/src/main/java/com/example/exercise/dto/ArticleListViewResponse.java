c30e1c278d0c08fdbaa5c5bdfdc318cd6a67ffff
Kiruchoco <sba1013kr@naver.com>
2024-02-05 12:00:51 +0900

    make blog page(CRUD)

---
+++ exercise/src/main/java/com/example/exercise/dto/ArticleListViewResponse.java
@@ -0,0 +1,17 @@
+package com.example.exercise.dto;
+
+import com.example.exercise.domain.Article;
+import lombok.Getter;
+
+@Getter
+public class ArticleListViewResponse {
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article){
+        this.id=article.getId();
+        this.title=article.getTitle();
+        this.content=article.getContent();
+    }
+}
